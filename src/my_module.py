6d556501c2eeb08646da4ef8518f31eac2a4e65f
Alexandre L Barbosa <alexandrelb.700@gmail.com>
2022-05-12 18:13:02 -0300

    cleaning input

--- src/my_module.py
+++ src/my_module.py
@@ -1,3 +1,14 @@
 def get_cheapest_hotel(number):   #DO NOT change the function's name
     cheapest_hotel = "cheapest_hotel_name"
+
+    split1 = number.split(':', 1)
+    type = split1[0]
+
+    split2 = split1.split(',', -1)
+    dates = split2.pop(0)
+
+    for d in dates:
+        pass
+
+
     return cheapest_hotel
